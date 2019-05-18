559badb533481846b347ac62538850561ba4f548
rakeshchil <chrakesh445@gmail.com>
2019-05-18 11:21:39 +0530

    first commit.....

---
+++ src/mmm/HI.java
@@ -0,0 +1,14 @@
+package mmm;
+
+public class HI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("......");
+		System.out.println("12133");
+		
+
+	}
+
+}
